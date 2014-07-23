c4c4a0a95a0faac6ea612e4473881f2a618ce9bb
nevik-xx <kflowers0101@yahoo.com>
2014-07-23 10:26:07 -0500

    ps3toolchain with a minor changes to build on OsX tested on 1.6.7 with Xcode 3.2-3.2.6

--- scripts/008-psl1ght.sh
+++ scripts/008-psl1ght.sh
@@ -2,10 +2,10 @@
 # psl1ght.sh by Dan Peori (dan.peori@oopo.net)
 
 ## Download the source code.
-wget --no-check-certificate https://github.com/ps3dev/PSL1GHT/tarball/master -O psl1ght.tar.gz
+clone clone https://github.com/nevik-xx/psl1ght.git
 
 ## Unpack the source code.
-rm -Rf psl1ght && mkdir psl1ght && tar --strip-components=1 --directory=psl1ght -xvzf psl1ght.tar.gz
+#rm -Rf psl1ght && mkdir psl1ght && tar --strip-components=1 --directory=psl1ght -xvzf psl1ght.tar.gz
 
 ## Create the build directory.
 cd psl1ght
